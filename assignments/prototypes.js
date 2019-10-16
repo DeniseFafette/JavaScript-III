c201da7511daa6db62ab8126db0428003a73c2a8
DeniseFafette <denisefafette@gmail.com>
2019-10-16 13:38:07 -0700

    set up prototypes

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -41,7 +41,18 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+function GameObject(charprofile){
+  this.newCreated = charprofile.createdAt,
+  this.newDimension = charprofile.dimensions,
+  this.newLength = charprofile.length,
+  this.newWidth = charprofile.width,
+  this.newHeight = charprofile.height
+}
+
+GameObject.prototype.dead = function(){
+  return `${this.name} was removed from the game.`
+};
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. (global binding) called in the global points to the window
+* 2. (implicit binding) this points to the object before the dot when a function is called by a preceeding dot - 
+* 3. (new binding) constructor function is used points this to the object - that is created and returned by that constructor function
+* 4. (explicit binding) call () apply() methods are explicitly defined and set what this points to
 *
 * write out a code example of each explanation above
 */
